3a3c71640da56363fb48b3ad9e77c0413b914ca5
Anuj-Kumar-152 <kumaranuj7794@gmail.com>
2026-03-14 23:50:21 +0530

    fix server.js

--- server.js
+++ server.js
@@ -10,43 +10,47 @@ const topicRoutes = require("./src/routes/topicRoutes.js");
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 9000;
-
-/* ---------------- Middleware ---------------- */
+const PORT = process.env.PORT || 9000; 
+const allowedOrigins = [
+   "http://localhost:5173",
+   "https://learn-stack-frontend-lilac.vercel.app"
+];
 
 app.use(
    cors({
-      origin: [
-         "http://localhost:5173", // frontend dev 
-         process.env.FRONTEND_URL // production frontend
-      ],
+      origin: function (origin, callback) { 
+         if (!origin) return callback(null, true);
+
+         if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+         } else {
+            return callback(new Error("CORS not allowed"));
+         }
+      },
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true
    })
 );
 
 app.use(express.json());
-
-/* ---------------- Routes ---------------- */
+ 
 
 app.get("/", (req, res) => {
-   res.send("🚀 Learn Stack API is running");
+   res.send("Learn Stack API is running");
 });
 
-app.use("/api", topicRoutes);
-
-/* ---------------- Database + Server ---------------- */
+app.use("/api", topicRoutes); 
 
 connectDb()
    .then(() => {
 
-      console.log("\n✅ Database connected successfully");
+      console.log("Database connected successfully");
 
       server.listen(PORT, () => {
-         console.log(`🚀 Server running on port ${PORT}`);
+         console.log(`Server running on port ${PORT}`);
       });
 
    })
    .catch((err) => {
-      console.error("❌ Database connection failed:", err);
+      console.error("Database connection failed:", err);
    });
